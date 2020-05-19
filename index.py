1e1560de9732f6f37227d2b6f2bc73e1f440eafb
Alberto <albertojcvs@gmail.com>
2020-05-19 18:51:19 -0300

    arrumando bug

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
-from selenium.webdriver.chrome.options import Options
+
 
 def getTrendTopics(trendTopicsHTML):
 
@@ -10,25 +10,28 @@ def getTrendTopics(trendTopicsHTML):
     soup = BeautifulSoup(trendTopicsHTML, 'html.parser')
 
     for trend in soup.find_all('div', dir='ltr'):
-        if trend.span != None :
+        if trend.span != None:
             trendTopicsList.append(trend.span.get_text())
-    
+            # print(trend.span.get_text())
+
     return trendTopicsList
 
+
 url = 'https://twitter.com/explore'
-option = Options()
-option.headless= True
-driver = webdriver.Chrome(options= option)
+driver = webdriver.Chrome()
 driver.get(url)
 sleep(8)
 
-driver.find_element_by_xpath('//a[@href="/explore/tabs/tab_1"]').click();
+driver.find_element_by_xpath('//a[@href="/explore/tabs/tab_1"]').click()
 
 sleep(5)
-trendTopicsHTML = driver.find_element_by_xpath('//div[@aria-label="Timeline: Explorar"]').get_attribute('outerHTML')
+trendTopicsHTML = driver.find_element_by_xpath(
+    '//div[@aria-label="Timeline: Explorar"]').get_attribute('outerHTML')
 driver.quit()
 
-trendTopicsList =  getTrendTopics(trendTopicsHTML)
+trendTopicsList = getTrendTopics(trendTopicsHTML)
 
-for trend in  trendTopicsList: 
+
+for trend in trendTopicsList:
     print(trend)
+# getTrendTopics(trendTopicsHTML)
