5df21ffc0db577a682f3c31ddff45439c59c0279
Alberto <albertojcvs@gmail.com>
2020-05-23 21:22:03 -0300

    adicionando a função de pegar os dados do  youtube

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Web Scraping com Python
 
 
-Código feito em python que pega a lista dos trending topics do twitter usando o Selenium e o BeautifulSoup
+Código feito em python que pega a lista dos assuntos do momento do twitter e a lista dos vídeos em alta do youtube usando o Selenium e o BeautifulSoup
--- index.py
+++ index.py
@@ -2,8 +2,19 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 
-def getTrendTopics(trendingTopicsHTML):
+def getTwitterTrendingTopics():
 
+    url = 'https://twitter.com/explore'
+    driver.get(url)
+    sleep(8)
+
+    driver.find_element_by_xpath('//a[@href="/explore/tabs/trending"]').click()
+
+    sleep(5)
+    trendingTopicsHTML = driver.find_element_by_xpath(
+        '//div[@aria-label="Timeline: Explorar"]').get_attribute('outerHTML')
+
+    driver.quit()
     trendingTopicsList = []
 
     soup = BeautifulSoup(trendingTopicsHTML, 'html.parser')
@@ -12,22 +23,43 @@ def getTrendTopics(trendingTopicsHTML):
         if topic.span != None:
             trendingTopicsList.append(topic.span.get_text())
 
-    return  trendingTopicsList
+    return trendingTopicsList
 
 
-url = 'https://twitter.com/explore'
+def getYoutubeTredingVideos():
+
+    url = 'https://www.youtube.com/feed/trending'
+    driver.get(url)
+    sleep(8)
+    trendingVideosHTML = driver.find_element_by_xpath(
+        '//div[@id="grid-container"]').get_attribute('outerHTML')
+    soup = BeautifulSoup(trendingVideosHTML, 'html.parser')
+
+    trendingVideosList = []
+    contador =0
+    for video in soup.find_all('a',id="video-title"):
+        trendingVideosList.append(video.get_text().strip())
+    
+    driver.quit()
+
+    return trendingVideosList
+
+
+
+option = int(input(' Digite [1] para ver os assuntos do momento do Twitter\n Digite [2]para ver os videos em alta do Youtube\n'))
+
+while option != 1 and option != 2:
+    print('Você digitou uma opção inválida!\n')
+    option = int(input(' Digite [1] para ver os assuntos do momento do Twitter\n Digite [2] para ver os videos em alta do Youtube\n'))
+
+
 driver = webdriver.Chrome()
-driver.get(url)
-sleep(8)
+if option == 1:
+    lista = getTwitterTrendingTopics()
+else:
+    lista = getYoutubeTredingVideos()
 
-driver.find_element_by_xpath('//a[@href="/explore/tabs/tab_1"]').click()
 
-sleep(5)
-trendingTopicsHTML = driver.find_element_by_xpath(
-    '//div[@aria-label="Timeline: Explorar"]').get_attribute('outerHTML')
-driver.quit()
 
-trendingTopicsList = getTrendTopics(trendingTopicsHTML)
-
-for topic in trendingTopicsList:
-    print(topic)
+for item in lista:
+    print(item)
